3b01bc587f6fa0db193b7b407d388bfe65191e06
tahrer007 <tahrer_Ad@outlook.com>
2022-01-14 23:16:28 +0200

    users data

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.get("",(req,res)=>{
-    res.send("hello tahrer !! ") ; 
-
-})
+app.get('/addUser', (req, res) => {
+    res.send(`<h1>add new user</h1>`)
+  });
 
 app.listen(3000,()=>{
     console.log("hello server 3000 !! ")
